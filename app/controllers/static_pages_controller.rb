f3767e0e0857666214037da403d0d5f1a02462f2
marcboanas <marcjohnboanas@gmail.com>
2014-02-01 12:22:27 +0000

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contact_us
+  end
+
+  def report_a_fault
+  end
+
+  def terms_conditions
+  end
+
+  def price_guide
+  end
+end
