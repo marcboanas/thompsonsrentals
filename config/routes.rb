6733c1d391c9b4d73c3222edb4841929651ab813
marcboanas <marcjohnboanas@gmail.com>
2014-02-02 13:53:22 +0000

    contact mailer sendgrid

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Thompsonsrentals::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :contacts
 
   # Sample resource route with options:
   #   resources :products do
