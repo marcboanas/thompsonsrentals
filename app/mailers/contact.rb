6b1cf5844906e12a150e0f89f4761d65f324985a
marcboanas <marcjohnboanas@gmail.com>
2014-02-02 15:08:19 +0000

    email

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -3,9 +3,6 @@ class Contact < ActionMailer::Base
     
   def contact_form(contact)
       @contact = contact
-      @contact.name = @contact.name ? @contact.name : "no name"
-      @contact.phone_number = @contact.phone_number ? @contact.phone_number : "no number"
-      @contact.message = @contact.message ? @contact.message : "message blank"
-      mail( :to => "marcjohnboanas@gmail.com", :from => @contact.name, :subject => "Thompsons Rentals Online Enquiry " + @contact.name + " " + @contact.phone_number)
+      mail( :to => "marcjohnboanas@gmail.com", :from => "Online Enquiry", :subject => "Thompsons Rentals Online Enquiry")
   end
 end
